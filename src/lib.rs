7380c5a5dfd4ef807713e63832290ed9359194db
Manuel Quarneti <mq1@ik.me>
2026-06-07 22:52:24 +0200

    optimize binary size

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,19 @@ include!(concat!(env!("OUT_DIR"), "/id_map_meta.rs"));
 
 #[repr(C)]
 struct Data {
-    pub game_ids: [u32; COUNT],
+    pub title_map_ids: [u32; TITLE_COUNT],
+    pub title_map_title_offsets: [u32; TITLE_COUNT + 1],
+
     #[cfg(feature = "gamehacking")]
-    pub ghids: [Option<std::num::NonZero<u32>>; COUNT],
-    pub title_offsets: [u32; COUNT + 1],
+    pub gamehacking_map_ids: [u32; GHID_COUNT],
+    #[cfg(feature = "gamehacking")]
+    pub gamehacking_map_ghids: [u32; GHID_COUNT],
+
     #[cfg(feature = "ascii-titles")]
-    pub ascii_title_offsets: [u32; COUNT + 1],
+    pub ascii_title_map_ids: [u32; ASCII_TITLE_COUNT],
+    #[cfg(feature = "ascii-titles")]
+    pub ascii_title_map_title_offsets: [u32; ASCII_TITLE_COUNT + 1],
+
     pub titles: [u8; TITLES_LEN],
 }
 
@@ -28,7 +35,7 @@ static DATA: std::sync::LazyLock<Box<Data>> = std::sync::LazyLock::new(|| {
 
     let mut buf = Box::<Data>::new_uninit();
 
-    // inflate
+    // inflate directly into the buffer
     let ptr = buf.as_mut_ptr().cast::<u8>();
     let slice = unsafe { std::slice::from_raw_parts_mut(ptr, std::mem::size_of::<Data>()) };
     let it = std::iter::once(compressed.as_slice());
@@ -37,56 +44,49 @@ static DATA: std::sync::LazyLock<Box<Data>> = std::sync::LazyLock::new(|| {
     unsafe { buf.assume_init() }
 });
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct GameEntry(usize);
+fn _get_title(game_id: u32) -> Option<&'static str> {
+    let index = DATA.title_map_ids.binary_search(&game_id).ok()?;
 
-impl GameEntry {
-    #[inline]
-    pub fn lookup(id: impl AsRef<str>) -> Option<Self> {
-        let id = u32::from_str_radix(id.as_ref(), 36).ok()?;
-        DATA.game_ids.binary_search(&id).ok().map(Self)
+    unsafe {
+        let start = *DATA.title_map_title_offsets.get_unchecked(index) as usize;
+        let end = *DATA.title_map_title_offsets.get_unchecked(index + 1) as usize;
+        let slice = DATA.titles.get_unchecked(start..end);
+        Some(std::str::from_utf8_unchecked(slice))
     }
+}
 
-    #[cfg(feature = "gamehacking")]
-    #[inline]
-    #[must_use]
-    pub fn ghid(&self) -> Option<std::num::NonZero<u32>> {
-        unsafe { *DATA.ghids.get_unchecked(self.0) }
+pub fn get_title(game_id: impl AsRef<str>) -> Option<&'static str> {
+    let game_id = u32::from_str_radix(game_id.as_ref(), 36).ok()?;
+    _get_title(game_id)
+}
+
+#[cfg(feature = "gamehacking")]
+fn _get_ghid(game_id: u32) -> Option<u32> {
+    let idx = DATA.gamehacking_map_ids.binary_search(&game_id).ok()?;
+    let ghid = unsafe { *DATA.gamehacking_map_ghids.get_unchecked(idx) };
+    Some(ghid)
+}
+
+#[cfg(feature = "gamehacking")]
+pub fn get_ghid(game_id: impl AsRef<str>) -> Option<u32> {
+    let game_id = u32::from_str_radix(game_id.as_ref(), 36).ok()?;
+    _get_ghid(game_id)
+}
+
+#[cfg(feature = "ascii-titles")]
+fn _get_ascii_title(game_id: u32) -> Option<&'static str> {
+    let idx = DATA.ascii_title_map_ids.binary_search(&game_id).ok()?;
+
+    unsafe {
+        let start = *DATA.ascii_title_map_title_offsets.get_unchecked(idx) as usize;
+        let end = *DATA.ascii_title_map_title_offsets.get_unchecked(idx + 1) as usize;
+        let slice = DATA.titles.get_unchecked(start..end);
+        Some(std::str::from_utf8_unchecked(slice))
     }
+}
 
-    #[inline]
-    #[must_use]
-    pub fn title(&self) -> &'static str {
-        #[cfg(not(feature = "compress"))]
-        let data = &DATA;
-
-        #[cfg(feature = "compress")]
-        let data = DATA.as_ref();
-
-        unsafe {
-            let start = *data.title_offsets.get_unchecked(self.0) as usize;
-            let end = *data.title_offsets.get_unchecked(self.0 + 1) as usize;
-            let slice = data.titles.get_unchecked(start..end);
-
-            std::str::from_utf8_unchecked(slice)
-        }
-    }
-
-    #[cfg(feature = "ascii-titles")]
-    #[inline]
-    #[must_use]
-    pub fn ascii_title(&self) -> &'static str {
-        unsafe {
-            let start = *DATA.ascii_title_offsets.get_unchecked(self.0) as usize;
-            let end = *DATA.ascii_title_offsets.get_unchecked(self.0 + 1) as usize;
-
-            if start == end {
-                self.title()
-            } else {
-                let slice = DATA.titles.get_unchecked(start..end);
-                std::str::from_utf8_unchecked(slice)
-            }
-        }
-    }
+#[cfg(feature = "ascii-titles")]
+pub fn get_ascii_title(game_id: impl AsRef<str>) -> Option<&'static str> {
+    let game_id = u32::from_str_radix(game_id.as_ref(), 36).ok()?;
+    _get_ascii_title(game_id).or_else(|| _get_title(game_id))
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@
 
 #![warn(clippy::all, rust_2018_idioms)]
 
-use twbm_idmap::GameEntry;
-
 const USAGE: &str = "Usage: twbm-idmap <GAMEID>";
 
 fn main() {
@@ -13,16 +11,18 @@ fn main() {
         std::process::exit(1);
     };
 
-    let Some(entry) = GameEntry::lookup(&game_id) else {
-        eprintln!("GameID {game_id} not found");
-        std::process::exit(1);
-    };
-
-    println!("Title: {}", entry.title());
+    let title = twbm_idmap::get_title(&game_id);
+    println!("Title: {title:?}");
 
     #[cfg(feature = "ascii-titles")]
-    println!("ASCII Title: {}", entry.ascii_title());
+    {
+        let ascii_title = twbm_idmap::get_ascii_title(&game_id);
+        println!("ASCII Title: {ascii_title:?}");
+    }
 
     #[cfg(feature = "gamehacking")]
-    println!("GameHacking ID: {:?}", entry.ghid());
+    {
+        let ghid = twbm_idmap::get_ghid(&game_id);
+        println!("GameHacking ID: {ghid:?}");
+    }
 }
